c7f394e00f1e0bb62e663bf9929ac02d29ff04b7
andres.armendariz@ioet.com <andres.armendariz@ioet.com>
2024-08-13 21:53:38 -0500

    test/AA: add tests for beverage_service

---
+++ app/test/services/test_beverage.py
@@ -0,0 +1,14 @@
+def test_create_beverage_service():
+    pass
+
+
+def test_update_beverage_service():
+    pass
+
+
+def test_get_beverage_by_id_service():
+    pass
+
+
+def test_get_beverages_service():
+    pass
